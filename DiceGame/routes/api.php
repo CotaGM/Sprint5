c03d63020bc716ca9e0a0c6852573dc2916f4859
CotaGM <cotadiseno@gmail.com>
2024-07-23 23:28:14 +0200

    getAverageSuccessRatedone and endpoint working

--- DiceGame/routes/api.php
+++ DiceGame/routes/api.php
@@ -15,38 +15,28 @@ Route::get('/test', function () {
 });
 
 // Open Routes
-Route::post('/players', [UserController::class, 'register']); //crea un jugador/a.
-Route::post('/login', [UserController::class, 'login']); //login
+Route::post('/players', [UserController::class, 'register']); // Crea un jugador/a
+Route::post('/login', [UserController::class, 'login']); // Login
 
-// Protected Routes
-Route::group([
-    "middleware" => ["auth:api"]
-], function(){
-    Route::get('/profile', [UserController::class, "profile"]); //
-    Route::get('/refresh-token', [UserController::class, "refreshToken"]);
-    Route::get('/logout', [UserController::class, "logout"]);
-    Route::put('/players/{id}', [UserController::class, "updateUser"]);
+// Protected routes
+Route::middleware(['auth:api'])->group(function () {
+    Route::get('/profile', [UserController::class, 'profile']);
+    Route::get('/refresh-token', [UserController::class, 'refreshToken']);
+    Route::get('/logout', [UserController::class, 'logout']);
+    Route::put('/players/{id}', [UserController::class, 'updateUser']);
+
+    // player
+    Route::middleware(['can:is-player'])->group(function () {
+        Route::post('/players/{id}/games', [GameController::class, 'throwDices']); // Un jugador/a específico realiza un tirón de los dados
+        Route::delete('/players/{id}/games', [GameController::class, 'deleteGames']);
+        Route::get('/players/{id}/games', [GameController::class, 'getGames']);
+    });
+
+    // Admin
+    Route::middleware(['can:is-admin'])->group(function () {
+        Route::get('/players', [UserController::class, 'getAllPlayers']);
+        Route::get('/players/ranking', [UserController::class, 'getAverageSuccessRate']); // Devuelve el ranking medio de todos los jugadores/as del sistema
+        Route::get('/players/ranking/loser', [UserController::class, 'getLoser']);
+        Route::get('/players/ranking/winner', [UserController::class, 'getWinner']);
+    });
 });
-
-// Player Routes
-Route::group([
-    'middleware' => ["auth:api"]
-], function () {
-    Route::post('/players/{id}/games', [GameController::class, 'throwDices']);//un jugador/a específico realiza un tirón de los dados.
-});
-
-
-//Route::get('/players', [UserController::class, 'getList']);
-//Route::post('/players', [UserController::class, 'register']);
-
-
-//Route::post('/players/{id}/games/', [PlayerController::class], "create game");//un jugador/a específico realiza un tirón de los dados.
-//Route::delete('/players/{id}/games/', [PlayerController::class], "delete games");//elimina las tiradas del jugador/a.
-
-//acceso administrador
-//Route::get('/players', [PlayerController::class, 'getList']);//devuelve el listado de todos los jugadores/as del sistema con su porcentaje medio de éxitos 
-//Route::get('/players/{id}/games', [PlayerController::class], "games list");//devuelve el listado de jugadas por un jugador/a.
-//Route::get('/players/ranking', [PlayerController::class], "ranking");//devuelve el ranking medio de todos los jugadores/as del sistema.  Es decir, el porcentaje medio de logros.
-//Route::get('/players/ranking/loser', [PlayerController::class], "looser");//devuelve al jugador/a con peor porcentaje de éxito.
-//Route::get('/players/ranking/winner', [PlayerController::class], "winner");//devuelve al jugador/a con mejor porcentaje de éxito .
-
