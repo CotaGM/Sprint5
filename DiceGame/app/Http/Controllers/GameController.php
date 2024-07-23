d01278f7cc821024e2f51f2bb4e74e0741116a13
CotaGM <cotadiseno@gmail.com>
2024-07-23 18:45:00 +0200

    adding again the throw dice function to see if the pull request worked

--- DiceGame/app/Http/Controllers/GameController.php
+++ DiceGame/app/Http/Controllers/GameController.php
@@ -9,6 +9,48 @@ use Illuminate\Support\Facades\Auth;
 
 class GameController extends Controller
 {
-   
+    public function throwDices(Request $request, int $id)
+    {
+        // find and verify user
+        $user = Auth::user();
+        $user = User::find($id);
+
+
+        if ($request->user()->id !== $user->id) {
+            return response()->json([
+                'message' => 'Unauthorized'
+            ]);
+        }
+
+        if (!$user) {
+            return response()->json([
+                'status' => false,
+                'message' => 'User not found',
+            ]);
+        }
+
+        // throw dice
+        $dice1 = rand(1, 6);
+        $dice2 = rand(1, 6);
+        $result = ($dice1 + $dice2) === 7;
+
+        //create game
+        $game = Game::create([
+            'user_id' => $user->id,
+            'dice1' => $dice1,
+            'dice2' => $dice2,
+            'result' => $result,
+        ]);
+
+        return response()->json([
+            'message' => 'Game created successfully',
+            'game' => [
+                'user_name' => $user->nickname,
+                'game' => $game,
+                'message' => $result ? 'You won!' : 'You lost.',
+            ]
+        ]);
+    }
+
     
 }
